4f7523a9d3581784710b9b8c2597382edb1a1d55
MarneusCalgarXP <marneuscalgarxp@gmail.com>
2019-11-27 14:16:44 +0100

    exo20

--- src/exo20.js
+++ src/exo20.js
@@ -4,12 +4,22 @@ export const query = array =>
   Object.assign(array, {
     where(key, condition) {
       //TODO: filtrer les éléments selon une condition sur une propriété
+      return query(this.filter(e => {
+        return condition.call(this, e[key]); 
+      }));
     },
     orderBy(key) {
       //TODO: trier les éléments selon une propriété
+      //return this.
+      let sortedValues = this.map(e => e[key]).sort();
+      let items = sortedValues.map(val => this.filter(e => e[key] === val));
+      let result = [];
+      items.forEach(subar =>  result.push(...subar));
+      return query(result);
     },
     take(number) {
       //TODO: retourner les N premiers éléments de la liste
+      return query(this.slice(0, number));
     }
   });
 
